6284b6c910a2b1071781bb8016f2c1bbcbbdcd07
JDASL <127839733+JDASL@users.noreply.github.com>
2025-02-03 13:35:28 +0100

    create the react app, updated bootstrap

---
+++ weather-react-week5/src/App.js
@@ -0,0 +1,37 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <footer>
+          <small>
+            <a
+              href="https://github.com/JDASL/weather-react-app.git"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open source code on
+              <a href="" target="_blank" rel="noreferrer">
+                GitHub
+              </a>{" "}
+            </a>
+            by{" "}
+            <em>
+              <a
+                href="https://www.instagram.com/yourgurljd/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                JD Apirinya🤩✨
+              </a>
+            </em>
+          </small>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
